0bac6ec9eef13452dd8f0165158e743543bcaaec
Jordan <jordancdiaz04@gmail.com>
2023-09-18 22:49:39 -0400

    add location data

---
+++ src/assets/ICECREAM_DATA.jsx
@@ -0,0 +1,76 @@
+import iceCream from "../assets/ice-cream-transparent.png";
+import Clevelands from "../assets/Clevelands.jpg";
+import Freddo from "../assets/Freddo.jpg";
+import MagicCow from "../assets/MagicCow.jpg";
+import SpikedRich from "../assets/SpikedRich.jpg";
+import FreezeN from "../assets/FreezeN.jpg";
+import Custard from "../assets/Custard.jpg";
+import Pinkberry from "../assets/Pinkberry.jpg";
+import Rosie from "../assets/Rosie.jpg";
+import Jeremiah from "../assets/Jeremiah.jpg";
+import Aloha from "../assets/Aloha.jpg";
+
+const ICE_CREAM_DATA = [
+  {
+    img: Clevelands,
+    name: "Clevelands Old Fashioned Ice Cream",
+    type: "Ice Cream",
+    menu: "https://www.yelp.com/biz/clevelands-old-fashioned-ice-cream-miramar",
+  },
+  {
+    img: Freddo,
+    name: "Freddo",
+    type: "Gelato",
+    menu: "https://usa.freddo.com/shop/",
+  },
+  {
+    img: MagicCow,
+    name: "The Magic Cow",
+    type: "Ice Cream + Froyo",
+    menu: "https://www.yelp.com/menu/the-magic-cow-davie-3",
+  },
+  {
+    img: SpikedRich,
+    name: "Spiked Rich Nitrogen Ice Cream",
+    type: "(Spiked) Nitrogen Ice Cream",
+    menu: "https://spikedrich.com/menu/",
+  },
+  {
+    img: FreezeN,
+    name: "Freeze N Nitro Creamery",
+    type: "Nitrogen Ice Cream",
+    menu: "https://frezennitrocreamery.square.site",
+  },
+  {
+    img: Custard,
+    name: "Siciliano's Frozen Custard",
+    type: "Frozen Custard",
+    menu: "https://www.yelp.com/menu/sicilianos-frozen-custard-hollywood",
+  },
+  {
+    img: Pinkberry,
+    name: "Pinkberry",
+    type: "Frozen Yogurt",
+    menu: "https://www.pinkberry.com/menu/flavors/index.php",
+  },
+  {
+    img: Rosie,
+    name: "Rosie's Gourmet Italian Ices",
+    type: "Ice Cream + Shaved Ice",
+    menu: "https://www.yelp.com/menu/rosies-gourmet-italian-ices-pembroke-pines",
+  },
+  {
+    img: Jeremiah,
+    name: "Jeremiah's Italian Ice",
+    type: "Gelati + Soft Ice Cream",
+    menu: "https://jeremiahsice.com/our-treats/gelati#",
+  },
+  {
+    img: Aloha,
+    name: "Sweet Aloha Ice Cream",
+    type: "Ice Cream + Shaved Ice",
+    menu: "https://www.sweetalohaicecream.com/menu",
+  },
+];
+
+export default ICE_CREAM_DATA;
